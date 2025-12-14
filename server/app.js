0a287b4348098e844686d5c78f64a63c9476e9a0
Jayveer-kumar <Jayveerk394@gmail.com>
2025-12-14 08:53:25 +0530

    Added a Root route for Server

--- server/app.js
+++ server/app.js
@@ -177,9 +177,14 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/", (req, res) => {
+    res.status(200).send("Backend is running 🚀");
+});
+
 
 app.use("/api/audio", audioRoutes);
 app.use("/api/email", emailRoutes);
+
 
 
 app.get("/testapi", (req, res) => {
