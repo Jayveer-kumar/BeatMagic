d72c60879a5aa9cff98251057b8afd45441684a7
Jayveer-kumar <Jayveerk394@gmail.com>
2026-09-10 12:16:09 +0530

    Changing Actual Server URL in Client side

--- Client/src/utils/Api.js
+++ Client/src/utils/Api.js
@@ -1,5 +1,5 @@
 // const API_URL = 'https://beatmagic.onrender.com' || 'http://localhost:8080';
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://beatmagic.onrender.com';
 console.log("API URL : ");
 console.log(API_URL);
 export function uploadAudio({ file, url, preset = "8d" , systemAddress, onProgress }) {
